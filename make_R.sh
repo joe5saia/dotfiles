04586c70b26f606e643ab6332308de081f99cbaa
Joe Saia <joe5saia@gmail.com>
2020-06-02 10:44:55 -0400

    updated versions

--- make_R.sh
+++ make_R.sh
@@ -4,6 +4,6 @@
 ## Installs R from CRAN repo
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran35/"
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 apt update
-apt install -y r-base-core r-base r-base-dev
+apt install -y r-base r-base-dev
