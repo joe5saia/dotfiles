e23f41034948946f35854ded98956bf1d0f1ed80
Joe Saia <joe5saia@gmail.com>
2021-02-09 17:25:49 -0500

    change install directory

--- make_python.sh
+++ make_python.sh
@@ -11,10 +11,10 @@ apt install -y build-essential
 apt install -y libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev tk-dev
 
 
-cd /usr/local/share
+cd ~
 wget https://www.python.org/ftp/python/${ver}/Python-${ver}.tar.xz
 tar xf Python-${ver}.tar.xz
 cd Python-${ver}
-./configure --enable-optimizations
+./configure --enable-optimizations --prefix=~/apps/python39
 make -j 10
-make altinstall
+make install
