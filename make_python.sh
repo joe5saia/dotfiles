0b587443c34586e602f3388697d875a3746c475c
Joe Saia <joe5saia@gmail.com>
2021-02-10 11:02:59 -0500

    Added sudo to apt commands

--- make_python.sh
+++ make_python.sh
@@ -5,16 +5,16 @@
 
 ver=3.9.1
 
-apt update
-apt upgrade -y
-apt install -y build-essential
-apt install -y libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev tk-dev
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y build-essential
+sudo apt install -y libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev libffi-dev uuid-dev tk-dev
 
 
 cd ~
 wget https://www.python.org/ftp/python/${ver}/Python-${ver}.tar.xz
 tar xf Python-${ver}.tar.xz
 cd Python-${ver}
-./configure --enable-optimizations --prefix=~/apps/python39
+./configure --enable-optimizations --prefix=/home/saiaj/apps/python39
 make -j 10
 make install
